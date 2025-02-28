b316f83473705f1470011b049faf69ffae8fdf17
Homero A Beltran <89602971+HomeroABeltran@users.noreply.github.com>
2025-02-28 10:33:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById("theme-toggle").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
-
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+    });
+});
